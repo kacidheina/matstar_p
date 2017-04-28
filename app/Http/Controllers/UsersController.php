aeb7689c5ef5c0bcba340359558a64db39f15a0f
Ina Kacidhe <ina.kacidhe@gmail.com>
2017-04-28 23:52:32 +0200

    Added Deleted Listed Users

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\User;
+use DB;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Auth;
 
 class UsersController extends Controller
 {
@@ -14,7 +17,8 @@ class UsersController extends Controller
      */
     public function index()
     {
-        //
+        $users =DB::select('SELECT c.*, uc.`name` AS `creator`, um.`name` AS `modifier`, c.`created_at`, c.`updated_at` FROM `users` c LEFT JOIN `users` uc ON c.`user_create_id`= uc.`id` LEFT JOIN `users` um ON c.`user_modify_id`= um.`id` WHERE c.`delete` = \'no\'');
+         return view('users.users_listing',['users' => $users]);
     }
 
     /**
@@ -24,7 +28,7 @@ class UsersController extends Controller
      */
     public function create()
     {
-        //
+        return view('users.add_user');
     }
 
     /**
@@ -35,7 +39,27 @@ class UsersController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validator = Validator::make($request->all(),
+            [
+                'name' => 'required|min:4',
+                'email' => 'required',
+                'status' => 'required',
+                'password' => 'required|min:8'
+            ]);
+        if ($validator->fails()) {return redirect()->back()->with('error','Te dhenat nuk u futen ne formatin e duhur.');}
+
+        $user = new User();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->status = $request->status;
+        $user->password = bcrypt($request->password);
+        $user->user_create_id = Auth::user()->id;
+        $user->created_at = date("Y-m-d H:i:s");
+
+        if ($user->save())
+        {return redirect('users')->with('success','Perdoruesi u shtua me sukses.');}
+        else
+        {return redirect()->back()->with('error','Dicka shkoi gabim. Provoni perseri.');}
     }
 
     /**
@@ -80,6 +104,12 @@ class UsersController extends Controller
      */
     public function destroy(User $user)
     {
-        //
+        if($user){
+            $user->delete = 'yes';
+            $user->user_modify_id = Auth::user()->id;
+            $user->updated_at = date('Y-m-d H:i:s');
+            $user->save();
+            return response()->json(['message' => 'Perdorues u fshi me sukses','error' => 'false']);
+        }
     }
 }
